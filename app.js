f1c173c267e13c7b3ab949d341b063a749529c4f
Dion Carlo Alano <skyfall.noid@gmail.com>
2017-10-05 01:23:17 +0800

    Update

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+var charactersData = require("./data/characters.json")
 
 var app = express();
-
 var port = process.env.PORT || 8080;
 
 //set body parser
@@ -21,33 +21,9 @@ app.get("/", function (req, res) {
 
 // select characters
 app.get("/api/characters/", function (req, res) {
-    var data = require("./data/characters.json")
-    res.send(JSON.stringify(data));
-});
-
-// select customer where id = ?
-app.get("/api/customers/:_id", function (req, res) {
-    db.find({ _id: req.params._id }, function (err, docs) {
-        res.json(docs[0]);
-    });
-});
-
-
-// insert into customers
-app.post("/api/customers", function (req, res) {
-    var user = req.body;
-    db.insert(user, function () {
-        res.json({ success: true });
-    });
-});
-
-// delete from customers where id = ?
-app.delete("/api/customers", function () {
-    // db.remove({ _id: req.params }, function (err, numRemoved) {
-    //     res.json({ success: true });
-    // });
+    res.send(JSON.stringify(charactersData));
 });
 
 app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+    console.log('Running on http://localhost:' + port);
 });
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
 var app = new Vue({
     el: "#app",
     data: {
+        toolbar: true,
+        spu: 5,
+        plusStat: 40,
+        selectedRarity: [],
+        rarities: ['1', '2', '3', '4', '5', '6'],
+        selectedElements: [],
+        elements: [
+            { text: 'Ardor' },
+            { text: 'Whirlwind' },
+            { text: 'Thunder' },
+            { text: 'Light' },
+            { text: 'Dark' }
+        ],
         characterHeaders: [
             { text: 'Name', align: 'left', value: 'name' },
+            { text: 'Rarity', value: 'rarity' },
             { text: 'Element', value: 'element' },
             { text: 'Max Power', value: 'max_pow' },
             { text: 'Max Technique', value: 'max_tec' },
@@ -11,25 +25,61 @@ var app = new Vue({
             { text: 'Average Stats', value: 'avg_stat' },
             { text: 'Total Stats', value: 'total_stat' }
         ],
-        "loading": "primary",
-        "search": '',
-        "pageRows": [25, 50, 100, { text: "All", value: -1 }],
+        loading: true,
+        search: '',
+        pageRows: [25, 50, 100, { text: "All", value: -1 }],
         characters: []
     },
-    created: function () {
+    mounted: function () {
         this.getCharData();
     },
     methods: {
+        isPlayer: function (character) {
+            return (character === "Player") ? true : false;
+        },
+        origin_stat: function (min, max, RARITY, LEVEL) {
+            rarity_scale_thing = Math.round(min * Math.pow(.176, (6 - RARITY) / 10));
+            return Math.floor(min + (max - rarity_scale_thing) * Math.pow((LEVEL - 1) / (60 - 1), 1))
+        },
+        superb_stat_bonus: function () {
+            return 1 + this.spu * 0.1;
+        },
         getCharData: function () {
+            this.loading = true
             axios.get('/api/characters/').then(response => {
                 var data = response.data
+                console.log(data)
                 for (var x = 0; x < data.length; x++) {
-                    data[x].total_stat = data[x].max_pow + data[x].max_tec + data[x].max_vit + data[x].max_spd;
-                    data[x].avg_stat = (data[x].max_pow + data[x].max_tec + data[x].max_vit + data[x].max_spd) / 4;
-                }
-                this.characters = data;
+                    if (this.isPlayer(data[x].category)) {
+                        var namePrefix = data[x].id.toString().substring(0, 1);
+                        if (namePrefix == "3") {
+                            namePrefix = " (EE)";
+                        }
+                        else if (namePrefix == "2") {
+                            namePrefix = " (E)";
+                        }
+                        else
+                            namePrefix = "";
+                        var max_pow = Math.floor(data[x].max_pow * this.superb_stat_bonus() + this.plusStat);
+                        var max_tec = Math.floor(data[x].max_tec * this.superb_stat_bonus() + this.plusStat);
+                        var max_vit = Math.floor(data[x].max_vit * this.superb_stat_bonus() + this.plusStat);
+                        var max_spd = Math.floor(data[x].max_spd * this.superb_stat_bonus() + this.plusStat);
+                        this.characters.push({
+                            name: data[x].name + namePrefix,
+                            rarity: data[x].rarity,
+                            element: data[x].element,
+                            max_pow: max_pow,
+                            max_tec: max_tec,
+                            max_vit: max_vit,
+                            max_spd: max_spd,
+                            total_stat: max_pow + max_tec + max_vit + max_spd,
+                            avg_stat: (max_pow + max_tec + max_vit + max_spd) / 4
+                        });
+                    }
+                };
                 this.loading = false;
             });
         }
     }
 })
+
