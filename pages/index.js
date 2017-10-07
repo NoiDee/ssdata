4cd52f15b1ccb00018d5a9be2d68260727feafa8
Dion Carlo Alano <skyfall.noid@gmail.com>
2017-10-07 17:11:17 +0800

    Fixes

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,32 @@
 var app = new Vue({
     el: "#app",
     data: {
-        toolbar: true,
-        spu: 5,
-        plusStat: 40,
+        bs: 40,
+        stats: {
+            bs: { value: 40, max: 40, color: 'light-green' },
+            spu: { value: 5, max: 5, color: 'deep-orange' },
+            apu: { value: 0, max: 30, color: 'cyan' }
+        },
         selectedRarity: [],
         rarities: ['1', '2', '3', '4', '5', '6'],
+        selectedRole: [],
         selectedElements: [],
-        elements: [
-            { text: 'Ardor' },
-            { text: 'Whirlwind' },
-            { text: 'Thunder' },
-            { text: 'Light' },
-            { text: 'Dark' }
-        ],
+        characters: [],
+        charactersTable: [],
         characterHeaders: [
             { text: 'Name', align: 'left', value: 'name' },
             { text: 'Rarity', value: 'rarity' },
+            { text: 'Role', value: 'role' },
             { text: 'Element', value: 'element' },
             { text: 'Max Power', value: 'max_pow' },
             { text: 'Max Technique', value: 'max_tec' },
             { text: 'Max Vitality', value: 'max_vit' },
             { text: 'Max Speed', value: 'max_spd' },
-            { text: 'Average Stats', value: 'avg_stat' },
             { text: 'Total Stats', value: 'total_stat' }
         ],
         loading: true,
         search: '',
-        pageRows: [25, 50, 100, { text: "All", value: -1 }],
-        characters: []
+        pageRows: [15, 30, 50]
     },
     mounted: function () {
         this.getCharData();
@@ -42,44 +40,36 @@ var app = new Vue({
             return Math.floor(min + (max - rarity_scale_thing) * Math.pow((LEVEL - 1) / (60 - 1), 1))
         },
         superb_stat_bonus: function () {
-            return 1 + this.spu * 0.1;
+            return 1 + this.stats.spu.value * 0.1;
         },
         getCharData: function () {
-            this.loading = true
             axios.get('/api/characters/').then(response => {
-                var data = response.data
-                console.log(data)
+                this.loading = false;
+                var data = response.data;
+                charactersTable = [];
                 for (var x = 0; x < data.length; x++) {
                     if (this.isPlayer(data[x].category)) {
-                        var namePrefix = data[x].id.toString().substring(0, 1);
-                        if (namePrefix == "3") {
-                            namePrefix = " (EE)";
+                        var base = data[x].id.toString().substring(0, 1);
+                        if (base == "3") {
+                            data[x].name += " (EE)";
                         }
-                        else if (namePrefix == "2") {
-                            namePrefix = " (E)";
+                        else if (base == "2") {
+                            data[x].name += " (E)";
                         }
-                        else
-                            namePrefix = "";
-                        var max_pow = Math.floor(data[x].max_pow * this.superb_stat_bonus() + this.plusStat);
-                        var max_tec = Math.floor(data[x].max_tec * this.superb_stat_bonus() + this.plusStat);
-                        var max_vit = Math.floor(data[x].max_vit * this.superb_stat_bonus() + this.plusStat);
-                        var max_spd = Math.floor(data[x].max_spd * this.superb_stat_bonus() + this.plusStat);
-                        this.characters.push({
-                            name: data[x].name + namePrefix,
+                        this.charactersTable.push({
+                            name: data[x].name,
                             rarity: data[x].rarity,
+                            role: data[x].role,
                             element: data[x].element,
-                            max_pow: max_pow,
-                            max_tec: max_tec,
-                            max_vit: max_vit,
-                            max_spd: max_spd,
-                            total_stat: max_pow + max_tec + max_vit + max_spd,
-                            avg_stat: (max_pow + max_tec + max_vit + max_spd) / 4
+                            max_pow: data[x].max_pow,
+                            max_tec: data[x].max_tec,
+                            max_vit: data[x].max_vit,
+                            max_spd: data[x].max_spd
                         });
                     }
                 };
-                this.loading = false;
+                // this.characters = data;
             });
         }
     }
-})
-
+});
